c7d9068526a2e2d6e0e59704e54710ba264fadbc
Clint <clinthiles1992@gmail.com>
2024-12-09 13:50:50 -0800

    Refactored to clean the commented out code

--- src/Components/MatrixAnimation.jsx
+++ src/Components/MatrixAnimation.jsx
@@ -4,17 +4,6 @@ import useMatrixAnimation from "../hooks/useMatrixAnimation";
 
 const MatrixAnimation = () => {
   const { messages, messageIndex } = useMatrixAnimation();
-  // const [messageIndex, setMessageIndex] = useState(0);
-  // const messages = ["Wake up, Neo..."];
-
-  // useEffect(() => {
-  //   if (messageIndex < messages.length) {
-  //     const timer = setTimeout(() => {
-  //       setMessageIndex((prevIndex) => prevIndex + 1);
-  //     }, 4000); // Wait for 4 seconds between messages
-  //     return () => clearTimeout(timer); // Cleanup timeout
-  //   }
-  // }, [messageIndex, messages.length]);
 
   return (
     <div className="matrix-container">
--- src/hooks/useMatrixAnimation.js
+++ src/hooks/useMatrixAnimation.js
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 
+// Custom hook to handle the matrix animation
+
 const useMatrixAnimation = () => {
+  // State to keep track of the current message index
+  // and the messages to display
   const [messageIndex, setMessageIndex] = useState(0);
   const messages = ["Wake up, Neo..."];
 
+  // Use effect to update the message index every 4 seconds
   useEffect(() => {
     if (messageIndex < messages.length) {
       const timer = setTimeout(() => {
         setMessageIndex((prevIndex) => prevIndex + 1);
-      }, 4000); // Wait for 4 seconds between messages
+      }, 4000);
       return () => clearTimeout(timer); // Cleanup timeout
     }
   }, [messageIndex, messages.length]);
