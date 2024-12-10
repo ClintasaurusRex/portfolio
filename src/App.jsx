a1dea1121d65df7f779ef6649f1da6a94afa01e5
Clint <clinthiles1992@gmail.com>
2024-12-10 13:02:59 -0800

    Refactored so that the animation runs before the cards pop up

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import MatrixAnimation from "./Components/MatrixAnimation";
 import Footer from "./Components/Footer";
-// import ProjectModal from "./Modal/ProjectModal";
 import ProjectGrid from "./Components/ProjectGrid";
 
 function App() {
+  const [showProjects, setShowProjects] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowProjects(true);
+    }, 8000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <header>Welcome to my Portfolio</header>
-      <div>
-        <MatrixAnimation />
-        <ProjectGrid />
-      </div>
       <div className="app">
+        <MatrixAnimation />
+        {showProjects && <ProjectGrid />}
         <Footer />
       </div>
     </>
