bd6de7379310d4254efbbec219b62afc2c5d4115
Clint <clinthiles1992@gmail.com>
2024-12-11 09:12:15 -0800

    added some mock data to the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,40 @@
 import MatrixAnimation from "../Components/MatrixAnimation";
 import Footer from "../Components/Footer";
-// import NavBar from "../Components/NavBar";
+import { NavLink } from "react-router-dom";
+import useTypingEffect from "../hooks/typingEffect";
+import "./home.css";
 
 function Home() {
+  const welcomeText = useTypingEffect("Welcome to my Portfolio");
   return (
     <>
-      <div className="app">
-        <MatrixAnimation />
-        <Footer />
+      <MatrixAnimation />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <h1 className="display-1">{welcomeText}</h1>
+            <p className="lead">
+              This is a simple portfolio website created using React. Feel free to explore the
+              website and check out my projects.
+            </p>
+            <nav className="nav-homebtns">
+              <NavLink to="/projects" className="btn btn-success">
+                View Projects
+              </NavLink>
+              <NavLink to="/about" className="btn btn-success">
+                About
+              </NavLink>
+              <NavLink to="/contact" className="btn btn-success">
+                Contact
+              </NavLink>
+            </nav>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <img src="https://via.placeholder.com/500" alt="placeholder" className="img-fluid" />
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
