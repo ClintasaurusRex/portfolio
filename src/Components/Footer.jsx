db2f3a45da5998d5cacbf3cf2eecd1602548131b
Clint <clinthiles1992@gmail.com>
2024-12-12 11:31:38 -0800

    Refactored footer to be green

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,22 @@
 import "./footer.css";
 
 const Footer = () => {
+  const successLink =
+    "link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover";
+
   return (
-    <footer>
-      <p>© 2024 Clint Arneson-Hiles. All rights reserved.</p>
+    <footer className="footer-container">
+      <p className={successLink}>© 2024 Clint Arneson-Hiles. All rights reserved.</p>
       <div className="footer-links">
-        <a href="https://github.com/ClintasaurusRex">GitHub</a>
-        <a href="https://www.linkedin.com/in/clint-arneson-hiles-042bb0321">LinkedIn</a>
-        <a href="mailto:clinthiles1992@gmail.com">Email Me</a>
+        <a href="https://github.com/ClintasaurusRex" className={successLink}>
+          GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/clint-arneson-hiles-042bb0321" className={successLink}>
+          LinkedIn
+        </a>
+        <a href="mailto:clinthiles1992@gmail.com" className={successLink}>
+          Email Me
+        </a>
       </div>
     </footer>
   );
