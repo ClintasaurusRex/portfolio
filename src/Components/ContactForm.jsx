aa668759f8f98ebeb61eb407ddded54544d1f38c
Clint <clinthiles1992@gmail.com>
2025-01-02 13:18:41 -0800

    Add ContactForm component with name, email, and message fields

---
+++ src/Components/ContactForm.jsx
@@ -0,0 +1,15 @@
+const ContactForm = () => {
+  return (
+    <form>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" />
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" />
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message" />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+export default ContactForm;
