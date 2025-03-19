3827ccaf7f1a1614fc8dcc9de22eb1c2e2f92c56
Clint <clinthiles1992@gmail.com>
2025-03-19 16:55:03 -0700

    changed up the projects section almost done

--- src/Projects/projects.js
+++ src/Projects/projects.js
@@ -1,51 +1,51 @@
 export const projects = [
   {
     id: 1,
-    title: "T.L.D.R",
-    summary: "A google chrome extension that summarizes articles.",
+    title: 'T.L.D.R',
+    summary: 'A google chrome extension that summarizes articles.',
     description:
-      "This extension summarizes and saves web articles for later reading. The extension also provides a text box that the user can talk directly to chatGPT with.",
+      'This extension summarizes and saves web articles for later reading. The extension also provides a text box that the user can talk directly to chatGPT with.',
     challenges:
-      "The biggest challenge was working with the chatGPT API and getting the extension to work with the API and this was our first and only extension that my partner and I made. Learning webpack and how to use it was also a challenge. This project uses Javascript, React, Chrome API, webpack, Scss and ChatGPT API.",
-    link: "https://chromewebstore.google.com/detail/tldr/fdoabojpkjopijhpfhapkddbafljjfff?hl=en-US&utm_source=ext_sidebar",
+      'The biggest challenge was working with the chatGPT API and getting the extension to work with the API and this was our first and only extension that my partner and I made. Learning webpack and how to use it was also a challenge. This project uses Javascript, React, Chrome API, webpack, Scss and ChatGPT API.',
+    link: 'https://chromewebstore.google.com/detail/tldr/fdoabojpkjopijhpfhapkddbafljjfff?hl=en-US&utm_source=ext_sidebar',
   },
   {
     id: 2,
-    title: "Joyland Prime Academy",
-    summary: "Fullstack application for a school in Kenya",
+    title: 'Joyland Prime Academy',
+    summary: 'Full-stack application for a school in Kenya',
     description:
-      "This project features a modern, responsive design built with React and Material UI components, ensuring an optimal user experience across various devices. Information about the school, including its establishment in 2008, is thoughtfully organized and presented clearly through a clean visual hierarchy using cards and papers to distinguish content sections. Its grid-based layout dynamically adjusts to different screen sizes, enhancing accessibility. In addition there is also a ton of admin capabilites that give the admin full control of the application",
+      'This project features a modern, responsive design built with React and Material UI components, ensuring an optimal user experience across various devices. Information about the school, including its establishment in 2008, is thoughtfully organized and presented clearly through a clean visual hierarchy using cards and papers to distinguish content sections. Its grid-based layout dynamically adjusts to different screen sizes, enhancing accessibility. In addition there is also a ton of admin capabilites that give the admin full control of the application',
     challenges:
-      "This was my first time working with firebase which just came with a learning curve, but the biggest challenge was this was also my first time using Material U.I, learning the syntax was a challenge but quite a lot of fun.",
-    link: "https://joyland.netlify.app/",
-  },
-  {
-    id: 3,
-    title: "Jungle",
-    summary: "Mock online shopping platform.",
-    description:
-      "Jungle is a mini e-commerce application built with Rails 6.1, designed to teach Rails by example. This application allows users to browse and search for products, manage their shopping cart, and securely checkout using Stripe. Additionally, it includes features for user authentication, account management, and an admin dashboard for inventory management.This project uses Ruby, HTML, Javascript and SCSS.",
-    challenges:
-      "Jungle was tricky because this was my first project using ruby on rails. Once I started to get the hang of how rails worked, it was a lot easier to understand how to implement the features that I wanted to add to the project and this was actually one of my favorite projects to work on.",
-    link: "https://github.com/ClintasaurusRex/jungle",
-  },
-  {
-    id: 4,
-    title: "PhotoLabs",
-    summary: "A mock photo gallery application.",
-    description:
-      "PhotoLabs is a modern, full-stack, responsive single-page application (SPA) built with React that allows users to explore, view, and interact with a collection of high-quality photographs. Key features include browsing a diverse collection of photos, navigating through different photo categories, viewing enlarged versions of photos with related images, liking photos, and responsive design for seamless use across devices. This project was built with React, JavaScript, HTML, Sass, Express, PostgreSQL.",
-    challenges:
-      "PhotoLabs was my very first React project which came with many challenges like understanding props and state. Otherwise a very fun project to work on. I also plan to revisit this project and add more features to it.",
-    link: "https://github.com/ClintasaurusRex/photoLab",
+      'This was my first time working with firebase which just came with a learning curve, but the biggest challenge was this was also my first time using Material U.I, learning the syntax was a challenge but quite a lot of fun.',
+    link: 'https://joyland.netlify.app/',
   },
   {
     id: 5,
-    title: "Siding Business",
-    summary: "Website for a siding business",
+    title: 'Siding Business',
+    summary: 'Front-end Website for a siding business',
     description:
-      "This app is a clean, modern, single-page design that serves as a professional online presence for a siding service business.“SG-Siding” conveys professionalism and quality craftsmanship. It successfully uses modern web design trends—minimalism, clear typography, and effective use of visual space—to create a user-friendly platform that highlights the core offerings of a siding business.",
-    link: "https://shaung.netlify.app/",
+      'This app is a clean, modern, single-page design that serves as a professional online presence for a siding service business.“SG-Siding” conveys professionalism and quality craftsmanship. It successfully uses modern web design trends—minimalism, clear typography, and effective use of visual space—to create a user-friendly platform that highlights the core offerings of a siding business.',
+    link: 'https://shaung.netlify.app/',
+  },
+  {
+    id: 4,
+    title: 'PhotoLabs',
+    summary: 'A Full-stack mock photo gallery application.',
+    description:
+      'PhotoLabs is a modern, full-stack, responsive single-page application (SPA) built with React that allows users to explore, view, and interact with a collection of high-quality photographs. Key features include browsing a diverse collection of photos, navigating through different photo categories, viewing enlarged versions of photos with related images, liking photos, and responsive design for seamless use across devices. This project was built with React, JavaScript, HTML, Sass, Express, PostgreSQL.',
+    challenges:
+      'PhotoLabs was my very first React project which came with many challenges like understanding props and state. Otherwise a very fun project to work on. I also plan to revisit this project and add more features to it.',
+    link: 'https://github.com/ClintasaurusRex/photoLab',
+  },
+  {
+    id: 3,
+    title: 'Jungle',
+    summary: 'Full-stack mock online shopping platform.',
+    description:
+      'Jungle is a mini e-commerce application built with Rails 6.1, designed to teach Rails by example. This application allows users to browse and search for products, manage their shopping cart, and securely checkout using Stripe. Additionally, it includes features for user authentication, account management, and an admin dashboard for inventory management.This project uses Ruby, HTML, Javascript and SCSS.',
+    challenges:
+      'Jungle was tricky because this was my first project using ruby on rails. Once I started to get the hang of how rails worked, it was a lot easier to understand how to implement the features that I wanted to add to the project and this was actually one of my favorite projects to work on.',
+    link: 'https://github.com/ClintasaurusRex/jungle',
   },
   // {
   //   id: 5,
