f8e98cbb1bf788996644d7d3e23953b1e777e763
Clint <clinthiles1992@gmail.com>
2025-03-19 20:21:32 -0700

    Fixed toggle bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,69 +1,73 @@
-import { NavLink } from "react-router-dom";
-import "./Navbar.css";
-import React from "react";
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/"></NavLink>
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <div className='container-fluid'>
+        <NavLink className='navbar-brand' to='/'></NavLink>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+        <div
+          className='collapse navbar-collapse justify-content-between'
+          id='navbarNav'
+        >
           <ul
-            className="nav nav-pills nav-fill gap-2 p-1 small rounded-5 shadow-sm custom-nav-pills"
-            id="pillNav2"
-            role="tablist"
+            className='nav nav-pills nav-fill gap-2 p-1 small rounded-5 shadow-sm custom-nav-pills'
+            id='pillNav2'
+            role='tablist'
           >
-            <li className="nav-item" role="presentation">
+            <li className='nav-item' role='presentation'>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? "nav-link active rounded-5" : "nav-link rounded-5"
+                  isActive ? 'nav-link active rounded-5' : 'nav-link rounded-5'
                 }
-                to="/"
-                role="tab"
+                to='/'
+                role='tab'
               >
                 Home
               </NavLink>
             </li>
-            <li className="nav-item" role="presentation">
+            <li className='nav-item' role='presentation'>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? "nav-link active rounded-5" : "nav-link rounded-5"
+                  isActive ? 'nav-link active rounded-5' : 'nav-link rounded-5'
                 }
-                to="/projects"
-                role="tab"
+                to='/projects'
+                role='tab'
               >
                 Projects
               </NavLink>
             </li>
-            <li className="nav-item" role="presentation">
+            <li className='nav-item' role='presentation'>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? "nav-link active rounded-5" : "nav-link rounded-5"
+                  isActive ? 'nav-link active rounded-5' : 'nav-link rounded-5'
                 }
-                to="/about"
-                role="tab"
+                to='/about'
+                role='tab'
               >
                 About
               </NavLink>
             </li>
-            <li className="nav-item" role="presentation">
+            <li className='nav-item' role='presentation'>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? "nav-link active rounded-5" : "nav-link rounded-5"
+                  isActive ? 'nav-link active rounded-5' : 'nav-link rounded-5'
                 }
-                to="/contact"
-                role="tab"
+                to='/contact'
+                role='tab'
               >
                 Contact
               </NavLink>
