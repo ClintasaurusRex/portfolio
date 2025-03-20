6a7bb02e321453660df30b7e3740791e5483292d
Clint <clinthiles1992@gmail.com>
2025-03-20 15:13:57 -0700

    added animation to contactme

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,38 +1,42 @@
-import "./contactForm.css";
-import useTypingEffect from "../hooks/typingEffect";
-import React from "react";
+import './contactForm.css';
+import useTypingEffect from '../hooks/typingEffect';
+import React from 'react';
+import MatrixAnimation from './MatrixAnimation';
 
 const ContactForm = () => {
   const typedText = useTypingEffect("Let's Connect!");
 
   return (
-    <div className="contact-container" data-testid="contact-container">
-      <h1 className="contact-title">{typedText}</h1>
-      <div className="contact-links">
-        <a href="mailto:clinthiles1992@gmail.com" className="contact-link">
-          <i className="bi bi-envelope-fill"></i>
-          <span>Email</span>
-        </a>
-        <a
-          href="https://github.com/ClintasaurusRex"
-          className="contact-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="bi bi-github"></i>
-          <span>GitHub</span>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/clint-arneson-hiles-042bb0321"
-          className="contact-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="bi bi-linkedin"></i>
-          <span>LinkedIn</span>
-        </a>
+    <>
+      <MatrixAnimation />
+      <div className='contact-container' data-testid='contact-container'>
+        <h1 className='contact-title'>{typedText}</h1>
+        <div className='contact-links'>
+          <a href='mailto:clinthiles1992@gmail.com' className='contact-link'>
+            <i className='bi bi-envelope-fill'></i>
+            <span>Email</span>
+          </a>
+          <a
+            href='https://github.com/ClintasaurusRex'
+            className='contact-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <i className='bi bi-github'></i>
+            <span>GitHub</span>
+          </a>
+          <a
+            href='https://www.linkedin.com/in/clint-arneson-hiles-042bb0321'
+            className='contact-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <i className='bi bi-linkedin'></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
