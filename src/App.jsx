a2d8bdb4195801c6e653ac9ab98603762106abe8
Clint <clinthiles1992@gmail.com>
2025-03-20 17:56:01 -0700

    Nothing to see here

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import Home from "./Pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Projects from "./Pages/Projects";
-import About from "./Pages/About";
+import './App.css';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Projects from './Pages/Projects';
+import About from './Pages/About';
 
-import NavBar from "./Components/NavBar";
-import CursorBubble from "./Components/CursorBubble";
-import ContactForm from "./Components/ContactForm";
+import NavBar from './Components/NavBar';
+import CursorBubble from '../src/CursorBub/CursorBubble';
+import ContactForm from './Components/ContactForm';
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
       <NavBar />
       <>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<ContactForm />} />
+          <Route path='/' element={<Home />} />
+          <Route path='projects' element={<Projects />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<ContactForm />} />
         </Routes>
       </>
     </Router>
