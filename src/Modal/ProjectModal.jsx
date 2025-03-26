49b254cb3664ac13ce4d82476f56efb6fd327554
Clint <clinthiles1992@gmail.com>
2025-03-26 09:02:42 -0700

    Enhanced ProjectCard component with hover effects to play/pause video previews on mouse events.

--- src/Modal/ProjectModal.jsx
+++ src/Modal/ProjectModal.jsx
@@ -1,7 +1,9 @@
 import './ProjectModal.css';
 import React from 'react';
+
 const ProjectModal = ({ project, onClose }) => {
   if (!project) return null;
+
   return (
     <div className='modal-backdrop' data-testid='modal-backdrop'>
       <div className='modal-content' data-testid='modal-content'>
@@ -25,14 +27,16 @@ const ProjectModal = ({ project, onClose }) => {
           ))}
         </div>
 
-        <a
-          href={project.link}
-          target='_blank'
-          rel='noopener noreferrer'
-          className='btn btn-outline-success btn-lg mt-3'
-        >
-          View Project
-        </a>
+        {project.title !== 'Wyatt Wiebe Wellness Center' && (
+          <a
+            href={project.link}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='btn btn-outline-success btn-lg mt-3'
+          >
+            View Project
+          </a>
+        )}
       </div>
     </div>
   );
